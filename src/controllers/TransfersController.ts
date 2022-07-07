05c4da23fbe568b489c8510ff09ec6cc99f9b312
FelipeDasr <felipe.heiker07@gmail.com>
2022-07-06 20:41:10 -0400

    TransferController implementation

--- src/controllers/TransfersController.ts
+++ src/controllers/TransfersController.ts
@@ -1,9 +1,78 @@
-import { IAuthenticatedRequestDTO } from '../dto/RequestsDTOs';
+import {
+    IAuthenticatedRequestDTO,
+    INewTransferRequestDTO
+} from '../dto/RequestsDTOs';
+import { IQueryDTO } from '../dto/QueryDTOs';
+
 import { NextApiResponse } from 'next';
+import { prisma } from '../lib/database';
 
-export const transferMoney = async (req: IAuthenticatedRequestDTO, res: NextApiResponse) => {
+export const transferMoney = async (req: INewTransferRequestDTO, res: NextApiResponse) => {
     try {
+        const { accountId: payerId, cash } = req.locals;
+        const { recipientId, value } = req.body;
 
+        if (value > cash) {
+            return res.status(200).json({
+                message: "Insufficient funds"
+            });
+        }
+
+        const recipient = await prisma.account.findFirst({
+            where: {
+                id: recipientId
+            }
+        });
+
+        if (!recipient) {
+            return res.status(400).json({
+                message: "The recipient does not exist"
+            });
+        }
+
+        const fieldsToSelect = {
+            name: true,
+            email: true,
+            cash: true,
+        }
+
+        const transactionResult = await prisma.$transaction([
+            prisma.account.update({
+                where: {
+                    id: payerId,
+                },
+                data: {
+                    cash: {
+                        decrement: value
+                    }
+                },
+                select: { ...fieldsToSelect }
+            }),
+            prisma.account.update({
+                where: {
+                    id: recipientId
+                },
+                data: {
+                    cash: {
+                        increment: value
+                    }
+                },
+                select: { ...fieldsToSelect, cash: false }
+            }),
+            prisma.transaction.create({
+                data: {
+                    payerId,
+                    recipientId,
+                    value
+                }
+            })
+        ]);
+
+        return res.status(200).json({
+            payer: transactionResult[0],
+            recipient: transactionResult[1],
+            transferData: transactionResult[2]
+        });
     }
     catch (e) {
         res.status(500).json({
@@ -14,7 +83,39 @@ export const transferMoney = async (req: IAuthenticatedRequestDTO, res: NextApiR
 
 export const getTransfers = async (req: IAuthenticatedRequestDTO, res: NextApiResponse) => {
     try {
+        const query = req.query as IQueryDTO;
+        const limit = query.limit ? Number(query.limit) : 50;
+        const page = query.page ? Number(query.page) * limit : 0;
 
+        const { accountId } = req.locals;
+
+        const fieldsToSelect = {
+            name: true,
+            email: true,
+        }
+
+        const transfers = await prisma.transaction.findMany({
+            where: {
+                OR: [
+                    { payerId: accountId },
+                    { recipientId: accountId }
+                ]
+            },
+            include: {
+                payer: {
+                    select: fieldsToSelect
+                },
+                recipient: {
+                    select: fieldsToSelect
+                }
+            },
+            take: limit,
+            skip: page
+        });
+
+        return res.status(200).json({
+            data: transfers
+        });
     }
     catch (e) {
         res.status(500).json({
